68d4709108084f3f4a822ae01eeed0a0fc8c2dc4
esraaMohamed <esraa.mohamed87@gmail.com>
2021-05-27 10:52:57 -0700

    Adding a UI change to test chromatic on PRs

--- src/stories/Button.js
+++ src/stories/Button.js
@@ -14,7 +14,7 @@ export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
       style={backgroundColor && { backgroundColor }}
       {...props}
     >
-      {label} on storybook
+      {label} Chromatic change 🪄
     </button>
   );
 };
